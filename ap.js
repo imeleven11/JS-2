d5f3d9c7e011c06813b11988c8a9436e5bb3c124
Muhammad Ahmed <155528907+imeleven11@users.noreply.github.com>
2024-03-04 13:39:11 +0500

    addition

---
+++ ap.js
@@ -0,0 +1,51 @@
+
+// ========================Chapter 01===========================
+
+// Q1
+alert ( "Error! Please enter a valid password. " );
+
+// Q2
+var valid_Password = "Error! Please enter a valid password.";
+alert(valid_Password);
+// Q3
+var hello = "Hello";
+var hi = "Hi";
+alert(hello + " \n " + hi);
+// Q4
+alert(hello);
+var dialog = "Prevent this page from additional dialogs.";
+alert(hi + " \n " + dialog );
+// Q5
+console.log("Hello...I can run JS through my web browser's console")
+
+// ========================Chapter 02===========================
+// Q1
+var username = "Muahmmad Ahmed";
+// Q2
+var myName = username;
+// Q3
+var message;
+message = "Hello World"
+alert(message);
+// Q4
+var studentname = "Muhammad Ahmed";
+var studentage = "19";
+alert(studentname);
+alert(studentage + " years old");
+// Q5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+
+// ========================Chapter 03===========================
+
+// Q1
+// number variable already declare
+alert("I am " + studentage + " year old");
+// Q2
+var visitors = 4;
+alert("You have visited this site " + visitors + " times");
+// Q3
+var yob = 2005;
+document.write("<br> My birth year is " + yob);
+// document.write("<br> Date type of my declare variable is number")
